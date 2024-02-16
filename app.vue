8991416552ec7eb8f3a5050f634ca818306791bc
Mahyar Maleki <mahyarmaleki2003@gmail.com>
2024-02-16 20:21:09 +0330

    initial commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} - Nuxt CRUD` : 'Nuxt CRUD';
+  }
+})
+</script>
+
+<template>
+  <v-app>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </v-app>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+useHead({
+  title: 'Home'
+})
+</script>
+
+<template>
+  <v-container fluid>
+    <h2>Home page</h2>
+  </v-container>
+</template>
+
+<style scoped>
+
+</style>
