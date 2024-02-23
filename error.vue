c7bdbdf0b207186f5751357ec4cc44031812e7c6
Mahyar Maleki <mahyarmaleki2003@gmail.com>
2024-02-23 16:14:04 +0330

    added custom 404 page not found component to handle wrong routes

---
+++ error.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps({
+  error: Object as () => NuxtError
+});
+const handleClearError = () => clearError({ redirect: '/' });
+</script>
+
+<template>
+  <v-container>
+    <v-row
+        align="center"
+        no-gutters
+    >
+      <v-col
+          cols="12"
+      >
+        <v-card elevation="10" >
+          <div class="text-center">
+            <v-card-title>{{ error.statusCode }}</v-card-title>
+            <v-card-text>{{ error.message }}</v-card-text>
+          </div>
+          <v-card-actions>
+            <v-btn
+                @click="handleClearError"
+                to="/"
+                color="primary"
+                variant="flat"
+                block
+            >Go back home</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+
+</style>
